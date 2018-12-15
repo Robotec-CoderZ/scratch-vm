81792cc703802199cc587e76844084a0f125a9be
Mohammad Qashoa <mohammad.m.q@gmail.com>
2018-12-14 18:43:02 -0800

    Add Robotec Extension

--- src/extension-support/extension-manager.js
+++ src/extension-support/extension-manager.js
@@ -16,7 +16,7 @@ const Scratch3TranslateBlocks = require('../extensions/scratch3_translate');
 const Scratch3VideoSensingBlocks = require('../extensions/scratch3_video_sensing');
 const Scratch3Speech2TextBlocks = require('../extensions/scratch3_speech2text');
 const Scratch3Ev3Blocks = require('../extensions/scratch3_ev3');
-
+const Scratch3RobotecBlocks = require('../extensions/scratch3_robotec');
 const builtinExtensions = {
     pen: Scratch3PenBlocks,
     wedo2: Scratch3WeDo2Blocks,
@@ -26,7 +26,8 @@ const builtinExtensions = {
     translate: Scratch3TranslateBlocks,
     videoSensing: Scratch3VideoSensingBlocks,
     speech2text: Scratch3Speech2TextBlocks,
-    ev3: Scratch3Ev3Blocks
+    ev3: Scratch3Ev3Blocks,
+    robotec: Scratch3RobotecBlocks
 };
 
 /**
